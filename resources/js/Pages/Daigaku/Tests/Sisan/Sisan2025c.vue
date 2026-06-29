807793bb038e58637d8716b43f31e94617f9911d
rencon0417 <yugo04172930@icloud.com>
2026-06-29 22:55:31 +0900

    アップデート

--- resources/js/Pages/Daigaku/Tests/Sisan/Sisan2025c.vue
+++ resources/js/Pages/Daigaku/Tests/Sisan/Sisan2025c.vue
@@ -613,7 +613,7 @@ const isDraft = true;
                             questionTitle: '円高・円安',
                             relatedProblems: ['2025b47', '2025c46'],
                             content: '円安（ドル高） ↔︎ 円高（ドル安）', //p108
-                        }, //47
+                        }, //46
                         {
                             // rev: 2026-06-29 確認済み
                             questionTitle: '外国投資信託の仕組み',
